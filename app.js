430b41122a9140e0b1d75d21b9b8cab26b35a827
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-05-31 12:41:49 +0100

    re-deploy to set api live

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require("express")
+const express = require("express");
 const { getTopics, getApi } = require("./controllers/topics.controllers");
 
-const { selectArticlebyId, getCommentsByArticleId, getArticles, updateArticleVotesById, postComment } = require("./controllers/articles.controllers");
+const {
+  selectArticlebyId,
+  getCommentsByArticleId,
+  getArticles,
+  updateArticleVotesById,
+  postComment,
+} = require("./controllers/articles.controllers");
 
 const endPoints = require("./endpoints.json");
 const { removeCommentById } = require("./controllers/comments.controllers");
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 
@@ -17,31 +23,28 @@ app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
 
-app.get("/api/articles/:article_id", selectArticlebyId)
+app.get("/api/articles/:article_id", selectArticlebyId);
 
+app.post("/api/articles/:article_id/comments", postComment);
 
-app.post("/api/articles/:article_id/comments", postComment)
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
+app.delete("/api/comments/:comment_id", removeCommentById);
 
+app.get("/api/articles", getArticles);
 
-app.delete("/api/comments/:comment_id", removeCommentById)
-
-app.get("/api/articles",  getArticles);
-
-app.patch("/api/articles/:article_id", updateArticleVotesById)
+app.patch("/api/articles/:article_id", updateArticleVotesById);
 
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.msg || "Internal Server Error";
-    if (err.code === "22P02") {
-        res.status(400).send({ msg: "Bad Request" })
-    } else if (err.code === "23502") {
-            res.status(400).send({ msg: "Bad Request Body" })
-        }
-        else if (err.code === "23503") {
-            res.status(404).send({ msg: "Not Found"})
-        }
-    res.status(status).send({ msg: message });
-})
+  const status = err.status || 500;
+  const message = err.msg || "Internal Server Error";
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else if (err.code === "23502") {
+    res.status(400).send({ msg: "Bad Request Body" });
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "Not Found" });
+  }
+  res.status(status).send({ msg: message });
+});
 module.exports = app;
